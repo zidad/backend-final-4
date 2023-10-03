29d28f0f532fbab4dfd44c2ca313427c2ab1fc8a
Hanna-Hinn <77826684+Hanna-Hinn@users.noreply.github.com>
2023-10-03 22:10:53 +0300

    Update index.js

--- models/index.js
+++ models/index.js
@@ -1,8 +1,8 @@
-const Address = require("./addressModel");
-const CartItem = require("./cartItemModel");
-const Cart = require("./cartModel");
-const Product = require("./productModel");
-const User = require("./userModel");
+const Address = require('./addressModel');
+const CartItem = require('./cartItemModel');
+const Cart = require('./cartModel');
+const Product = require('./productModel');
+const User = require('./userModel');
 
 module.exports = {
   Address,
