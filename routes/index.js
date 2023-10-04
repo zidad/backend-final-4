df6e2bbb80a9f1f912bfb8f39c49f8055f390dcf
Hanna-Hinn <77826684+Hanna-Hinn@users.noreply.github.com>
2023-10-04 19:33:00 +0300

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -2,13 +2,16 @@ const addressRoutes = require('./addressRoutes');
 const cartRoutes = require('./cartRoutes');
 const productRoutes = require('./productRoutes');
 const userRoutes = require('./userRoutes');
+const paymentRoutes = require('./paymentRoutes');
+const orderRoutes = require('./orderRoutes');
 const ratingReviewRoutes = require('./ratingReviewRoutes');
-
 
 module.exports = {
   addressRoutes,
   cartRoutes,
   productRoutes,
   userRoutes,
+  paymentRoutes,
+  orderRoutes,
   ratingReviewRoutes,
 };
