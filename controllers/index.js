5a2138c7f87c5f9f7a38fa9b4d5caeebc649d331
Hanna-Hinn <77826684+Hanna-Hinn@users.noreply.github.com>
2023-10-04 19:36:23 +0300

    Update index.js

--- controllers/index.js
+++ controllers/index.js
@@ -2,10 +2,18 @@ const productController = require('./productController');
 const userController = require('userController');
 const cartController = require('cartController');
 const addressController = require('./addressController');
+const ratingReviewController = require('./ratingReviewController');
+const addressController = require('./addressController');
+const paymentController = require('paymentController');
+const orderController = require('./orderController');
+
 
 module.exports = {
   productController,
   userController,
   cartController,
   addressController,
+  ratingReviewController,
+  paymentController,
+  orderController,
 };
