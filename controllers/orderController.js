ca5e49ae930ab73225924bbabc64bcab9de3c00e
Hanna-Hinn <hanna.hinn30@gmail.com>
2023-10-05 11:42:40 +0300

    made the order create based on the cart

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -1,5 +1,5 @@
 // imports
-const { Order, OrderItem } = require('../models');
+const { Cart, Order, OrderItem } = require('../models');
 const { asyncWrapper } = require('../middleware');
 const { createCustomError } = require('../utils/errors/custom-error');
 
@@ -26,17 +26,22 @@ const fetchOrders = asyncWrapper(async (req, res, next) => {
 });
 
 //  Create Order
-const createOrder = asyncWrapper(async (req, res) => {
+const createOrder = asyncWrapper(async (req, res, next) => {
   console.log('Creating Order');
-  const userId = 3;
+  const userId = 6;
   const { status } = req.body;
-  const totalPrice = Number(req.body.totalPrice);
   const tax = Number(req.body.tax);
   const deliveryFee = Number(req.body.deliveryFee);
   const paymentId = Number(req.body.paymentId);
   const cartId = Number(req.body.cartId);
   const addressId = Number(req.body.addressId);
 
+  const cart = await Cart.findByPk(cartId); // cartId is the same as the userId (incase of cartId is changed to multiple)
+  const cartItems = await cart.getCartItems();
+  if (cartItems.length === 0) {
+    return next(createCustomError(`Can not Place order on empty cart`, 500));
+  }
+  const totalPrice = Number(cart.totalPrice) + tax + deliveryFee;
   const order = await Order.create({
     totalPrice,
     status,
@@ -47,6 +52,16 @@ const createOrder = asyncWrapper(async (req, res) => {
     userId,
     addressId,
   });
+
+  for (const item of cartItems) {
+    await OrderItem.create({
+      price: item.price,
+      quantity: item.quantity,
+      productId: item.productId,
+      orderId: order.id,
+    });
+  }
+
   console.log('Order created', order.id);
   res.status(200).json({
     success: true,
@@ -108,9 +123,6 @@ const deleteOrder = asyncWrapper(async (req, res, next) => {
     .status(200)
     .json({ success: true, message: 'Order deleted successfully' });
 });
-
-
-
 
 //exports
 module.exports = {
