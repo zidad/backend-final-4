78d60d4354f30a69943de353a24ae309e5515227
Hanna-Hinn <77826684+Hanna-Hinn@users.noreply.github.com>
2023-10-17 19:00:44 +0300

    Update 20231005184520-add-rating-review-seeder.js

--- seeders/20231005184520-add-rating-review-seeder.js
+++ seeders/20231005184520-add-rating-review-seeder.js
@@ -7,19 +7,31 @@ const { ratingReviewData } = require('../utils/constants');
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    return await Promise.all(ratingReviewData.map(async data => {
-      try {
-        await RatingReview.findOrCreate({
-          where: { id: data.id },
-          defaults: data,
-        });
-      } catch (error) {
-        console.error(error);
-      }
-    }));
+    return await Promise.all(
+      ratingReviewData.map(async (data) => {
+        try {
+          const [rating, created] = await RatingReview.findOrCreate({
+            where: { id: data.id },
+            defaults: data,
+          });
+
+          // Update the product's virtual property
+          if (created) {
+            const product = await rating.getProduct();
+            product.setDataValue(
+              'totalRating',
+              product.totalRating + rating.rating
+            );
+            await product.save();
+          }
+        } catch (error) {
+          console.error(error);
+        }
+      })
+    );
   },
 
   async down(queryInterface, Sequelize) {
     return await queryInterface.bulkDelete('RatingReview', null, {});
-  }
+  },
 };
