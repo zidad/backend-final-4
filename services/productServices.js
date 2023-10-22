2b223e195937a38562d1b9523fbb58a60d4bf594
Hanna-Hinn <hanna.hinn30@gmail.com>
2023-10-22 15:51:55 +0300

    added ProductService

---
+++ services/productServices.js
@@ -0,0 +1,97 @@
+const { Product } = require('../models');
+
+/**
+ * Retrieve a product's rating summary, including the total rating and rating count.
+ *
+ * @param {Product} product - The product for which to retrieve the rating summary.
+ * @returns {Promise<{totalRating: number, ratingCount: number}>} An object containing the total rating and rating count.
+ */
+const getProductRatingSummary = async (product) => {
+  const reviews = await product.getRatingReviews();
+
+  const totalRating = reviews
+    ? (reviews
+        .map((review) => review.rating)
+        .reduce((acc, current) => acc + current, 0) /
+        reviews.length /
+        5) *
+      5
+    : 0;
+
+  const ratingCount = reviews ? reviews.length : 0;
+  return {
+    totalRating,
+    ratingCount,
+  };
+};
+
+/**
+ * Generate a structured response for a product, including details and rating summary.
+ *
+ * @param {Product} product - The product for which to generate a response.
+ * @returns {Promise<object>} A structured response object containing product details and rating summary.
+ */
+const generateProductResponse = async (product) => {
+  const { totalRating, ratingCount } = await getProductRatingSummary(product);
+
+  return {
+    id: product.id,
+    title: product.title,
+    description: product.description,
+    price: product.price,
+    availableInStock: product.availableInStock,
+    imageUrl: product.imageUrl,
+    category: product.category.name, // Access the category name
+    brand: product.brand.name, // Access the brand name
+    totalRating,
+    ratingCount,
+    discount: {
+      description: product.discount.description, // Access the discount description
+      percentage: product.discount.discountPercentage, // Access the discount percentage
+    },
+  };
+};
+
+/**
+ * Fetch a list of products with additional information and rating summaries.
+ *
+ * @param {object} options - Options for querying products.
+ * @returns {Promise<{products: object[], count: number}>} An object containing an array of products and the total count.
+ */
+const fetchProductsWithCount = async (options) => {
+  const products = await Product.findAll(options);
+  let count = 0;
+
+  // Transform the products data to include category name, brand name, and discount description/percentage
+  const transformedProducts = products.map(async (product) => {
+    // Get the totalRating and ratingCount
+    count++;
+    return await generateProductResponse(product);
+  });
+
+  //   Wait for all promises to resolve
+  const responseData = await Promise.all(transformedProducts);
+
+  return {
+    products: responseData,
+    count: count,
+  };
+};
+
+/**
+ * Fetch a product by its ID and generate a structured response.
+ *
+ * @param {number} id - The ID of the product to fetch.
+ * @param {object} options - Options for querying the product.
+ * @returns {Promise<object>} A structured response object containing product details and rating summary.
+ */
+const fetchProductById = async (id, options) => {
+  const product = await Product.findByPk(id, options);
+
+  return await generateProductResponse(product);
+};
+
+module.exports = {
+  fetchProductsWithCount,
+  fetchProductById,
+};
